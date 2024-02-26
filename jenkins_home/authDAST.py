faaa56ed54f2cf592d932a2f018863a6e6eeb555
Enrico Carteciano <134707381+DevRico003@users.noreply.github.com>
2024-02-26 19:18:47 +0100

    Update authDAST.py

--- jenkins_home/authDAST.py
+++ jenkins_home/authDAST.py
@@ -7,72 +7,69 @@ import random
 import string
 
 def randomString(stringLength=10):
+    """Generiert einen zufälligen String der angegebenen Länge."""
     letters = string.ascii_letters
     return ''.join(random.choice(letters) for i in range(stringLength))
 
 def bash_command(cmd):
+    """Führt einen Bash-Befehl aus."""
     subprocess.Popen(cmd, shell=True, executable='/bin/bash')
 
-chrome_options = Options()
+if len(sys.argv) < 4:
+    print('1. Geben Sie die IP-Adresse des Selenium Remote-Servers an!')
+    print('2. Geben Sie die IP-Adresse für den Ziel-DAST-Scan an!')
+    print('3. Geben Sie den Speicherort der HTML-Ausgabedatei an!')
+    sys.exit(1)
 
-# Erstellen Sie die DesiredCapabilities
-capabilities = webdriver.DesiredCapabilities.CHROME.copy()
-# Fügen Sie die ChromeOptions zur Capabilities hinzu
-capabilities.update(chrome_options.to_capabilities())
+# Konfigurieren Sie Chrome-Optionen nach Bedarf
+chrome_options = Options()
+# Beispiel: Führen Sie Chrome im Headless-Modus aus
+chrome_options.add_argument("--headless")
+
+# Erstellen des WebDriver-Objekts mit Remote-Optionen
+driver = webdriver.Remote(
+    command_executor=f"http://{sys.argv[1]}:4444/wd/hub",
+    options=chrome_options
+)
+
+# Geht zur Login-Seite
+driver.get(f"http://{sys.argv[2]}:10007/login")
+
+# Registriert einen neuen Benutzer
+register_button = driver.find_element_by_xpath("/html/body/div/div/div/form/center[3]/a")
+register_button.click()
 
 myusername = randomString(8)
 mypassword = randomString(12)
 
-if len(sys.argv) < 4:
-    print('1. Provide the ip address for selenium remote server!')
-    print('2. Provide the ip address for target DAST scan!')
-    print('3. Provide the output location of html report!')
-    sys.exit(1)
-
-driver = webdriver.Remote("http://"+sys.argv[1]+":4444/wd/hub", capabilities)
-
-driver.get("http://"+sys.argv[2]+":10007/login")
-
-registerbutton = driver.find_element_by_xpath("/html/body/div/div/div/form/center[3]/a")
-registerbutton.click()
-print("we're at: " + driver.current_url)
-
-print("creating a user..")
 username = driver.find_element_by_name("username")
 password1 = driver.find_element_by_name("password1")
 password2 = driver.find_element_by_name("password2")
 
-username.clear()
 username.send_keys(myusername)
-password1.clear()
 password1.send_keys(mypassword)
-password2.clear()
 password2.send_keys(mypassword)
 password2.send_keys(Keys.RETURN)
-login = driver.find_element_by_xpath('/html/body/div/div/div/center[2]/h4')
-assert "Login" in login.text
 
-print("created user")
-
-driver.get("http://"+sys.argv[2]+":10007/login")
-print("we're at: " + driver.current_url)
+# Loggt sich mit dem neuen Benutzer ein
+driver.get(f"http://{sys.argv[2]}:10007/login")
 username = driver.find_element_by_name("username")
 password = driver.find_element_by_name("password")
-username.clear()
+
 username.send_keys(myusername)
-password.clear()
 password.send_keys(mypassword)
 password.send_keys(Keys.RETURN)
-header = driver.find_element_by_xpath("/html/body/div/div/div[1]/h1")
-assert "Last gossips" in header.text
-print("logged in successfully.. getting cookie")
 
+# Extrahiert Cookies für den DAST-Scan
 nikto_string = "STATIC-COOKIE="
 cookies_list = driver.get_cookies()
 for cookie in cookies_list:
-    nikto_string+= '\"' + cookie['name'] + '\"='
-    nikto_string+= '\"'+ cookie['value'] + '\"'
-bash_command("cp /etc/nikto/config.txt ~/nikto-config.txt")
-bash_command("echo '" + nikto_string +"' >> ~/nikto-config.txt")
-print("added cookie to nikto config file to carry out authenticated scan..")
-bash_command("nikto -ask no -config ~/nikto-config.txt -Format html -h http://"+sys.argv[2]+":10007/gossip -output "+ sys.argv[3])
+    nikto_string += f"\"{cookie['name']}\"=\"{cookie['value']}\";"
+
+# Fügt den Cookie-String zur Nikto-Konfigurationsdatei hinzu
+bash_command(f"echo '{nikto_string}' > ~/nikto-config.txt")
+
+# Führt den Nikto-Scan durch
+bash_command(f"nikto -ask no -config ~/nikto-config.txt -Format html -h http://{sys.argv[2]}:10007/gossip -output {sys.argv[3]}")
+
+driver.quit()
