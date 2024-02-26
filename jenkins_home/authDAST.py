847b47c4b63378b2c6343b2d6e8a7a1330c959fc
Enrico Carteciano <134707381+DevRico003@users.noreply.github.com>
2024-02-26 19:25:41 +0100

    Update authDAST.py

--- jenkins_home/authDAST.py
+++ jenkins_home/authDAST.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import subprocess
 import sys
 import random
